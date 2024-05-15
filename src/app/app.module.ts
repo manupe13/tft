7f2734a7119e3dd28251da4a199c6c56f284a6e6
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 12:08:12 +0100

    Añadido el MatDialogModule e importación de ConfirmarDialogComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { environment } from "../environments/environment";
 import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { RouterModule } from '@angular/router';
 import { AccountComponent } from './pages/account/account.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { IngredientsComponent } from './pages/ingredients/ingredients.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MatDialogModule } from '@angular/material/dialog';
 import { PantryComponent } from './pages/pantry/pantry.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -25,11 +27,13 @@ import { UpdatePasswordComponent } from './pages/update-password/update-password
         AngularFirestoreModule,
         provideFirebaseApp(() => initializeApp(environment.firebase)),
         provideAuth(() => getAuth()),
+        MatDialogModule,
         provideStorage (() => getStorage())
     ],
     declarations: [
         AppComponent,
         AccountComponent,
+        ConfirmDialogComponent,
         ForgotPasswordComponent,
         FooterComponent,
         HeaderComponent,
