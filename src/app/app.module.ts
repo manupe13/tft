ff9a738a1e1a7a5c471dc2fed2ad7183bc4fcb6b
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 11:15:15 +0100

    Cambio de importación del AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routes';
 import { importProvidersFrom, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
