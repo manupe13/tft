447913a9f2a94371d8c0297f2bae206d464e4fc6
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 12:10:18 +0100

    Frontend de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +11,51 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  visiblePasswd: boolean = false;
+
+  user: any = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private navCtl: Router, private dialog: MatDialog) { }
+
+  loginUser() {
+
+  }
+
+  visibilityChange() {
+    const eyeControl = document.querySelector('#visibility');
+    const passwdField = document.querySelector('#user-password') as HTMLInputElement;
+    if (this.visiblePasswd) {
+      this.visiblePasswd = false;
+      eyeControl?.classList.add('fa-eye-slash');
+      eyeControl?.classList.remove('fa-eye');
+      passwdField.type = 'password';
+    } else {
+      this.visiblePasswd = true;
+      eyeControl?.classList.add('fa-eye');
+      eyeControl?.classList.remove('fa-eye-slash');
+      passwdField.type = 'text';
+    }
+  }
+
+  loginGoogle() {
+
+  }
+
+  openDialog(msg: string) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '500px',
+      data: msg
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        history.back();
+      }
+    });
+  }
 
 }
