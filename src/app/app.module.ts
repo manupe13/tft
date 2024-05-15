5ebbf57519f1d813ada889743e8d20f222e5e6d5
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 09:16:13 +0100

    Añadido las importaciones y declaraciones de Cuenta, Ingredientes, Recetas y Despensa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AngularFireModule} from '@angular/fire/compat';
 import { environment } from "../environments/environment";
 import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { RouterModule } from '@angular/router';
+import { AccountComponent } from './pages/account/account.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PantryComponent } from './pages/pantry/pantry.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UpdatePasswordComponent } from './pages/update-password/update-password.component';
 @NgModule({
@@ -25,11 +29,15 @@ import { UpdatePasswordComponent } from './pages/update-password/update-password
     ],
     declarations: [
         AppComponent,
+        AccountComponent,
         ForgotPasswordComponent,
         FooterComponent,
         HeaderComponent,
         HomeComponent,
+        IngredientsComponent,
         LoginComponent,
+        PantryComponent,
+        RecipesComponent,
         RegisterComponent,
         UpdatePasswordComponent,
 
