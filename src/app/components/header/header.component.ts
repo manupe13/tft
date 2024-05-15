9c8dcb9eda9ac8b4455904c0d9d4c9c6f243961a
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 11:14:09 +0100

    Rutas añadidas en el header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(private navCtl: Router) { }
 
 }
