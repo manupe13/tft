82d701887866d48156836df35ff6b847d80209cb
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 14:35:06 +0100

    Frontend de forgot-password

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResetPasswdService } from '../../services/reset-passwd.service';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,5 +13,39 @@ import { Component } from '@angular/core';
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
+
+  constructor() { }
+
+  /*userEmail = new FormControl('');
+
+  constructor(private resetPasswdSvc:ResetPasswdService,
+              private router:Router,
+              private dialog: MatDialog) { }
+
+  ngOnInit(): void { }
+
+  async onReset() {
+
+    try {
+      const email = this.userEmail.value!!;
+      await this.resetPasswdSvc.resetPassword(email);
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        width: '500px',
+        data: 'Correo electrónico enviado, ¡comprueba tu bandeja de entrada!'
+      });
+
+      dialogRef.afterClosed().subscribe((result) => {
+        this.router.navigate(['/login']);
+      });
+    } catch(error) {
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        width: '500px',
+        data: 'El correo electrónico no existe, inténtelo de nuevo.'
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        // Do nothing
+      });
+    }
+  }*/
 
 }
