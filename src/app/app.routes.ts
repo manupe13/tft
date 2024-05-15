29b5c7a6cc0b3eab15648ccf7fe6a6312a7722ca
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 09:42:47 +0100

    Importación de las rutas de los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,58 @@
-import { Routes } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './pages/account/account.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PantryComponent } from './pages/pantry/pantry.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UpdatePasswordComponent } from './pages/update-password/update-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'account',
+    component: AccountComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'ingredients',
+    component: IngredientsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'pantry',
+    component: PantryComponent
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'update-password',
+    component: UpdatePasswordComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
