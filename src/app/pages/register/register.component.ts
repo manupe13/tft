121bcbf40329fc2629dddefb045f655e691806e8
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 21:24:38 +0100

    Cabecera de register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-register',
@@ -8,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  constructor(private navCtl:Router,private dialog: MatDialog) { }
+
+  openDialog(event: Event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '500px',
+      data: '¿Estás seguro de que quieres retroceder? Perderá los datos añadidos'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.navCtl.navigate(['login']);
+      }
+    });
+  }
 
 }
