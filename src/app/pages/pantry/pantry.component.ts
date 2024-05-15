952c7cca663a5ac726c33757fd8de630d09f4c4f
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-15 09:07:01 +0100

    Creaación de los componentes de las páginas Cuenta, Ingredientes, Recetas y Despensa

---
+++ src/app/pages/pantry/pantry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantry',
+  standalone: true,
+  imports: [],
+  templateUrl: './pantry.component.html',
+  styleUrl: './pantry.component.css'
+})
+export class PantryComponent {
+
+}
