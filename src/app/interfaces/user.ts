a7993ee0fec14a16cb3ddc2bd2f00d51bcc65a70
manupe13 <emmanuel.pena101@alu.ulpgc.es>
2024-05-17 20:49:32 +0100

    Creacion de user interface

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+  nombre?: String,
+  apellido1?: String,
+  apellido2?: String,
+  date?: String,
+  country?: String,
+  phone?: String,
+  email?: string,
+  password?: string,
+  rol?: string,
+  existentes?: string[],
+  consumidos?: string[],
+  id?: string,
+}
